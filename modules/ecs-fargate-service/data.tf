d11a9fc1bbc2c627873bd27ad696a51924803f5c
rkbs <renukaraju.kbs@dxc.com>
2022-11-27 13:10:36 +0530

    Updated 14

--- modules/ecs-fargate-service/data.tf
+++ modules/ecs-fargate-service/data.tf
@@ -2,6 +2,7 @@ data "aws_caller_identity" "current" {}
 
 data "aws_vpc" "selected" {
   id = var.vpc_id
+  default = true
 }
 
 data "aws_subnets" "public" {
@@ -10,14 +11,23 @@ data "aws_subnets" "public" {
     values = [data.aws_vpc.selected.id]
   }
   filter {
+    name   = "default-for-az"
+    values = [true]
+  }
+  filter {
     name   = "state"
     values = ["available"]
   }
+data "aws_subnet" "public" {
+  for_each = { for index, subnetid in data.aws_subnets.public.ids : index => subnetid }
+  id       = each.value
+}
  #vpc_id = data.aws_vpc.selected.id
  tags = {
     Name = "*Public*"
   }
 }
+
 
 
 data "aws_availability_zones" "zones" {
